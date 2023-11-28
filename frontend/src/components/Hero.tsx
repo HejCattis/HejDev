3eb71226f8341fa48bcd33cd8218ad9087afd864
Cattis Gustafsson <cattis@hejdev.se>
2023-11-28 18:37:47 +0100

    Final design hero

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -4,16 +4,16 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 const Hero = () => {
 	return (
 		<React.Fragment>
-			<section className='max-w-4xl flex flex-col items-center mt-32 h-full'>
-				<div className='hidden md:block md:absolute md:-top-32 md:w-2/3 md:-right-52 max-w-3xl -z-10'>
+			<section className='max-w-4xl flex flex-col items-center mt-32 h-full mx-4'>
+				<div className='absolute -top-20 md:-top-32 w-[350px] md:w-2/3 -right-28 md:-right-52 max-w-3xl -z-10'>
 					<img src='/BG.png' alt='hero image' />
 				</div>
-				<div className="flex flex-col">
+				<div className="flex flex-col items-center md:items-start">
 					<h1 className='md:z-10 mb-4'>
 						{'<'} Hej {'/>'}
 					</h1>
-					<h2 className="mb-2">Mitt namn är <strong className="font-semibold">Cattis Gustafsson</strong></h2>
-					<p className="">Jag är en <strong className="font-semibold">Fullstack Webbutvecklare</strong></p>
+					<h2 className="text-center mb-2">Mitt namn är <strong className="font-semibold">Cattis Gustafsson</strong></h2>
+					<p className="text-center">Fullstack webbutvecklare med en touch av designer.</p>
 				</div>
 			</section>
 			<MdKeyboardArrowDown size={35} className='mx-auto mt-20 md:mt-32 lg:mt-56 text-purple' />
