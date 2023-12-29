403fc50049010bb885a232ce7958a9b25a6b0c3c
Cattis <cattis@hejdev.se>
2023-12-29 17:52:37 +0100

    Footer started

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,7 @@
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div className=' h-[200px] relative border-t-2 border-purple'>
+        </div>
+    );
 };
 export default Footer;
